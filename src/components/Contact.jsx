7e41f9dae3195a6a86feffd23bb16b692a6ba789
Gagan  Gupta <gagan.gupta@wakencode.com>
2024-04-03 23:31:06 +0530

    buils is added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,41 +1,49 @@
 import React from "react";
 import { useState } from "react";
-import './about.css'
-import AOS from "aos";
+import './about.css' // Importing the CSS file for the 'Contact' component
+import AOS from "aos"; // Importing the AOS (Animate On Scroll) library
 import { useEffect } from "react";
-import "aos/dist/aos.css";
-import emailjs from "emailjs-com";
+import "aos/dist/aos.css"; // Importing the CSS file for the AOS library
+import emailjs from "emailjs-com"; // Importing the emailjs library
+
 const Contact = () => {
+  // Initializing AOS and refreshing it on component mount
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
-  const [statehide , hideshow] = useState(false)
-  const [statehide2 , hideshow2] = useState(true)
+
+  // State for controlling the visibility of the form and the success message
+  const [statehide, hideshow] = useState(false);
+  const [statehide2, hideshow2] = useState(true);
+
+  // Function for handling form submission
   const submitmail = (e) => {
-    e.preventDefault();
-    emailjs.sendForm('service_58kptpq', 'template_e69lnql', e.target , 'WeNZZmpEv7GAZ48g3').then(res => {
+    e.preventDefault(); // Preventing the page from refreshing on form submission
+
+    // Sending the form data using emailjs
+    emailjs.sendForm('service_58kptpq', 'template_e69lnql', e.target, 'WeNZZmpEv7GAZ48g3').then(res => {
       console.log(res);
     }).catch(e => console.log(e));
+
+    // Hiding the form and showing the success message
     hideshow(true);
     hideshow2(false);
-    
-
-
   }
-  const showagain = () =>{
+
+  // Function for re-showing the form when the 'Edit Submission' button is clicked
+  const showagain = () => {
     hideshow(false);
     hideshow2(true);
-
-
   }
+
   return (
  
     <div 
       name="contact"
       className="contact_bg w-full h-auto bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
-      <div id={ statehide ? "contact_hide" : null} className="flex flex-col pt-8 justify-center max-w-screen-lg mx-auto h-full">
+      <div id={ statehide? "contact_hide" : null} className="flex flex-col pt-8 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
@@ -73,7 +81,7 @@ const Contact = () => {
           </form>
         </div>
       </div>
-      <div id={ statehide2 ? "contact_message" : null}>
+      <div id={ statehide2? "contact_message" : null}>
         <p className="message">Thank you for Contacting me. I will reach you very soon <button onClick={showagain} className="BUTTON_GLN">Edit Submission</button></p>
         {/* <p className="message2"></p> */}
       </div>
